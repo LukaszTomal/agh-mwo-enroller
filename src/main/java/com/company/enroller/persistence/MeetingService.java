e523e156e8c87400f16ffc84433437cb9f2ddd61
tomal <tomal_l@poczta.onet.pl>
2022-05-01 16:08:36 +0200

    Implementation meetings was done: Wersja BASIC 1.Pobieranie listy wszystkich spotkan 2.Pobieranie listy pojedyncznego spotkania 3.Dodawanie spotkan Wersja GOLD (dodatkowo do BASIC) 1.Usuwanie spotkan 2.Aktualizację spotkan

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import com.company.enroller.model.Participant;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,28 @@ public class MeetingService {
 		String hql = "FROM Meeting";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public Meeting findById(long id){
+		return(Meeting) connector.getSession().get(Meeting.class, id);
+	}
+
+	public void add(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+	}
+
+	public void delete(Meeting meeting){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(meeting);
+		transaction.commit();
 	}
 
 }
